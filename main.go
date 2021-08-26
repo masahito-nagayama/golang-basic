df789ea60257fe84a441317e2905814a1734654e
masahito-nagayama <me18sf67m@gmail.com>
2021-08-26 09:26:52 +0900

    basic-training

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"time"
 )
 
-// 複数の
-// コメント
-
 func main() {
 	fmt.Println("Hello World")
+
 	fmt.Println(time.Now())
+
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello Go"
+	fmt.Println(s)
+
+	var t,f bool = true,false
+	fmt.Println(t,f)
+
+	var (
+		si = 100
+		i2 = "Hello Golang"
+	)
+	fmt.Println(si,i2)
 }
