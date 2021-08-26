ef67b3dfdd1f3dbe7bdf54a2156a20f0e64ffe79
masahito-nagayama <me18sf67m@gmail.com>
2021-08-27 08:38:11 +0900

    add byte型

--- index.go
+++ index.go
@@ -8,4 +8,13 @@ func main() {
 	var i int = 100
 	
 	fmt.Println(i + 50)
+
+	var t, f bool = true,false
+	fmt.Println(t, f)
+
+	var s string = "HELLO GO LANG"
+	fmt.Println(s)
+
+	byteA := []byte{72, 73}
+	fmt.Println(byteA)
 }
